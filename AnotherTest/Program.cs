f4c3c6e72ecd3282fc56c605bad861d193e5d3dc
rachelKuroneko <jingyi-tan@hotmail.com>
2017-03-08 09:31:46 +1000

    Added Exception Handler for ReadFromFile method

--- AnotherTest/Program.cs
+++ AnotherTest/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
+
 namespace AnotherTest
 {
     class Program
@@ -40,19 +41,35 @@ namespace AnotherTest
 
         public static void ReadFromFile(TestBook[] obj, string path)
         {
-            StreamReader sr = new StreamReader(path);
+            StreamReader sr = null;
+            try 
+            { 
+                sr = new StreamReader(path);
 
-            for (int index = 0; index < obj.Length; index++)
-            {
-                // new instance in the element of the array 
-                obj[index] = new TestBook();
-                // start assigning values into the instances from the text file 
-                // reading each line sequentially 
-                obj[index].Title = sr.ReadLine();
+                for (int index = 0; index < obj.Length; index++)
+                {
+                    // new instance in the element of the array 
+                    obj[index] = new TestBook();
+                    // start assigning values into the instances from the text file 
+                    // reading each line sequentially 
+                    obj[index].Title = sr.ReadLine();
+                }
             }
-
-            //essential to put to close the stream reader off
-            sr.Close();
+            catch (FileNotFoundException ex)
+            {
+                //Log the details to the DB
+                Console.WriteLine("Please check if the file {0} exists", ex.FileName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            if (sr != null)
+            {
+                //essential to put to close the stream reader off
+                sr.Close();
+            }
+            Console.WriteLine("Finally Block");
         }
 
         // This method is using bubble sort that compares each pair of objects and swaps them in
